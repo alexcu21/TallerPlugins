41792645ab9a73870128bd972d34b42884881792
Alex Cuadra <alexcu21@live.com>
2023-05-23 22:21:34 -0600

    iniciando el plugin con los headers

---
+++ tallerplugins.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Taller de plugins
+Plugin URI: URI de tu complemento
+Description: Plugin que consume la informacion de la api de openweather
+Version: 1.0.0
+Author: Alex Cuadra
+Author URI: Tu Sitio Web
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+Text Domain: dominio-de-texto-de-tu-complemento
+*/
